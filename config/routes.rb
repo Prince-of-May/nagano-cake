5894743e5764259c0c2169d0f325991a3885f0f3
kobayashi9127 <bacchi.9127@gmail.com>
2021-05-18 12:35:59 +0000

    [2]

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
-  
+
+  root to: 'public/homes#top'
+  get '/admin' => 'admin/homes#top'
+
+  #adminの設定
  devise_for :admin, controllers: {
   sessions: 'admin/sessions'
 }
-  devise_for :customers
-  
   #customerの設定
-  
+  devise_for :customers
+
   #adminの設定
-  
+  namespace :admin do
+  resources :products, only: [:index, :show, :new, :create, :edit, :update]
+  end
+
+  #customerの設定
+  namespace :public do
+  end
+
 end
