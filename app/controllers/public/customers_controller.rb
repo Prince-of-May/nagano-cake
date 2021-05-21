b925ab3dd80a284e76d4e6e1feacfc92b0021ed3
takaki-homma <hmmtkk1994@gmail.com>
2021-05-21 04:13:21 +0000

    注文

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,8 @@
 class Public::CustomersController < ApplicationController
   
   def show
+    @customer = Customer.find
+    
   end 
   
   def edit
@@ -14,4 +16,13 @@ class Public::CustomersController < ApplicationController
   
   def withdraw
   end 
+  
+  private
+  #params.require(:キー(モデル名)).permit(:カラム名１,：カラム名２,・・・)
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_rubi, :first_name_rubi, :email, :postal_code, :address, :phone_number)
+  end
+  
+  
+  
 end
