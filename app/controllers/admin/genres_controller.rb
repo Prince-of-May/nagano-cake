d9fc12775640db2f3f8b6e7b67a9fc37377f2f9f
Ikumi Sugita <ikumi.sugita@icloud.com>
2021-05-22 05:43:16 +0000

    admin genres 追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,23 +7,27 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
+    @genre = Genre.find(genre_params)
   end
 
   def update
+    @genre = Genre.find(genre_params)
+    redirect_to admin_genres_path
   end
-  
+
   private
-  
-  
+
+
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
-  
+
+
 
 
 end
