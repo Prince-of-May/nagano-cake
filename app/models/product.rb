63c2a69973ee8fa2f07f7a553418abf37a0d2d51
kobayashi9127 <bacchi.9127@gmail.com>
2021-05-22 08:05:22 +0000

    編集

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,7 @@ class Product < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
   has_many :ordering_products, dependent: :destroy
-  belongs_to :genre, optional: true
-  # 応急処置
+  belongs_to :genre
 
   attachment :image
 end
