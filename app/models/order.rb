2881e441d790bf5d6bfd29bd3a38dea08c22f77b
Ikumi Sugita <ikumi.sugita@icloud.com>
2021-05-23 16:34:54 +0000

    enum 追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,5 +13,9 @@ class Order < ApplicationRecord
     "登録済住所から選択": 1,
     "新しいお届け先": 2,
   }
+  
+  enum status: [ "入金待ち", "入金確認", "製作中", "発送準備中", "発送済み" ]
+  
+  enum making_status: [ "着手不可", "製作待ち", "製作中", "製作完了"]
 end
 
