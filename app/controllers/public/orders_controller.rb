620f4f6f75f89b5b8adb62eb5e74f97213668b0f
takaki-homma <hmmtkk1994@gmail.com>
2021-05-23 15:59:16 +0000

    1

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,25 +1,31 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
   end
-  
+
   def confirm
-    
+
   end
-  
+
   def complete
   end
-  
+
   def create
     @order = Order.new(order_params)
-  end 
-  
+  end
+
   def index
     @orders = current_customer.orders#ログインユーザーのordersモデルを全て取得する（命名規則により、モデル名の前にメソッドがある場合ordersの頭文字は小文字にする）
-  end 
-  
+  end
+
   def show
-    #@orders = Order.where(:)
-  end 
+    @orders = current_customer.orders
+    @delivery_charge = 800
+  end
 end
+
+private
+  def order_params
+    params.require(:order).permit(:shipping_cost, :total_payment, :address, :postal_code, :name, :status, :customer_id )
+  end
