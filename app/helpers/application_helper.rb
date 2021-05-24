ef2b8d1d4983ddb0ba6f8cb592851a3b070cd65d
kobayashi9127 <bacchi.9127@gmail.com>
2021-05-24 06:48:39 +0000

    編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def tax_price(price)
     (price*1.1).floor
   end
+  
+  def sub_price(sub)
+    tax_price(sub.product.price) * sub.amount.to_i
+  end
+
+  def total_price(totals)
+    price = 0
+    totals.each do |total|
+      price += sub_price(total)
+      # price = price+数字　（＋＝）
+    end
+    return price
+  end
+  
 end
