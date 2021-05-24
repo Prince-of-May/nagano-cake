374b0b83bd170feaee10ff14e5a3417f65728212
yusa0410 <yusaku0410ishida@gmail.com>
2021-05-24 05:24:34 +0000

    1

--- app/controllers/admin/ordering_products_controller.rb
+++ app/controllers/admin/ordering_products_controller.rb
@@ -4,16 +4,16 @@ class Admin::OrderingProductsController < ApplicationController
     @ordering_product = OrderingProduct.find(params[:id])
     @ordering_product.update(ordering_product_params)
     @order = @ordering_product.order
-    
+
     #if @ordering_product.product_status == "製作中"
     #@ordering_product.order.update(making_status: "製作中")
     #redirect_to admin_order_path(@ordering_product.id)
   end
-  
+
   private
-  
+
   def ordering_product_params
     params.require(:ordering_product).permit(:making_status)
   end
-  
+
 end
